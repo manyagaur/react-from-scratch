8f69ed01c1783fef22f2c5fcfc8b3527761a9617
manyagaur <manyagaur25@gmail.com>
2024-05-23 19:38:12 +0530

    added changes to ListofFood

--- src/ListofFood.jsx
+++ src/ListofFood.jsx
@@ -4,16 +4,19 @@ function ListofFood(){
     const [food,setFood]= useState(["Chaap", "Pizza", "Burger"])
     
     function handleFood(){
-        const newFood=document.getElementById("newFood")
-        console.log(newFood)
-        setFood(...food,newFood)
+        const newFoodname=document.getElementById("newFood").value
+        console.log(newFoodname)
+        setFood([...food,newFoodname])
+        document.getElementById("newFood").value=""
     }
 
     return(
     <>
+    
     <h1>List of Food (Rendering Lists)</h1>
+    {console.log(food)}
     <ul>
-        {food.map((food,index)=><li key={index}>{food}</li>)}
+        {food.map((food,index)=>(<li key={index}>{food}</li>))}
     </ul>
     <input type="text" id="newFood" placeholder="Enter food name"></input>
     <button type="button" onClick={handleFood}>Add food</button>
